22e0d8df2d1279f43b52c56d8e2b6982d980241c
Jared Prather <jprather7966@yahoo.com>
2019-08-02 13:38:10 -0500

    Add project files.

---
+++ DoItWebsite/Models/TaskModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoItWebsite.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+    }
+}
