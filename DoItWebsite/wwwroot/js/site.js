79528499cf6cd22d52cdee25894a9d972176b5e1
Jared Prather <jprather7966@yahoo.com>
2019-08-08 11:38:51 -0500

    Removed unneeded CSS and used JavaScript to check empty inputs

--- DoItWebsite/wwwroot/js/site.js
+++ DoItWebsite/wwwroot/js/site.js
@@ -1,5 +1,20 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+//Checks if input if given is empty
+function validateForm()
+{
+	var inputValue = document.getElementById("myInput").value;
+	if (isEmptyOrSpaces(inputValue))
+	{
+		alert("Name must be filled out");
+		return false;
+	}
+	return true;
+}
+//Checks if empty input has multiple spaces 
+function isEmptyOrSpaces(str)
+{
+	return str === null || str.match(/^ *$/) !== null;
+}
 
